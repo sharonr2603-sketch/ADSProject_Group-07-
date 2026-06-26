247580a7b7d3bf16f6476ce83853ecae10da1c45
sharonr2603-sketch <sharonr2603@gmail.com>
2026-06-26 17:31:59 +0200

    updated

--- 03_RV32I-pipeline/src/main/scala/common.scala
+++ 03_RV32I-pipeline/src/main/scala/common.scala
@@ -23,17 +23,14 @@ package core_tile
 import chisel3._
 import chisel3.experimental.ChiselEnum
 
-// uopc means micro-operation code.
-// Decode stage converts RISC-V instruction into one of these internal operations.
+
 object uopc extends ChiselEnum {
 
-  // NOP means no operation.
-  // Used as default/safe instruction.
+
   val NOP,
 
-      // R-type operations
       ADD, SUB, SLL, SLT, SLTU, XOR, SRL, SRA, OR, AND,
 
-      // I-type operations
+
       ADDI, SLTI, SLTIU, XORI, ORI, ANDI, SLLI, SRLI, SRAI = Value
 }
